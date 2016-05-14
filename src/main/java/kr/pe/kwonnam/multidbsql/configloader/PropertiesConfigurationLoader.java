23f21ea1aad3c334fc7c6e43b9513db3327af546
KwonNam Son <kwon37xi@gmail.com>
2016-05-14 23:29:22 +0900

    prepare configuration loader

--- src/main/java/kr/pe/kwonnam/multidbsql/configloader/PropertiesConfigurationLoader.java
+++ src/main/java/kr/pe/kwonnam/multidbsql/configloader/PropertiesConfigurationLoader.java
@@ -7,9 +7,14 @@ import kr.pe.kwonnam.multidbsql.DatabaseGroup;
  * *.properties file are not required to be encoded with {@code native2ascii}.
  */
 public class PropertiesConfigurationLoader implements DatabaseConfigurationLoader {
+    public static final String LOADABLE_EXTENSION = ".properties";
+
     @Override
     public boolean checkLoadable(String argument) {
-        return false;
+        if (argument == null) {
+            return false;
+        }
+        return argument.endsWith(LOADABLE_EXTENSION);
     }
 
     @Override
--- src/test/java/kr/pe/kwonnam/multidbsql/configloader/PropertiesConfigurationLoaderTest.java
+++ src/test/java/kr/pe/kwonnam/multidbsql/configloader/PropertiesConfigurationLoaderTest.java
@@ -1,11 +1,36 @@
 package kr.pe.kwonnam.multidbsql.configloader;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PropertiesConfigurationLoaderTest {
     private Logger log = LoggerFactory.getLogger(PropertiesConfigurationLoaderTest.class);
+
+    private PropertiesConfigurationLoader loader;
+
+    @Before
+    public void setUp() throws Exception {
+        loader = new PropertiesConfigurationLoader();
+    }
+
+    @Test
+    public void checkLoadable_properties_true() throws Exception {
+        assertThat(loader.checkLoadable("hello.properties")).isTrue();
+        assertThat(loader.checkLoadable("something_config.properties")).isTrue();
+    }
+
+    @Test
+    public void checkLoadable_false() throws Exception {
+        assertThat(loader.checkLoadable(null)).isFalse();
+        assertThat(loader.checkLoadable("")).isFalse();
+        assertThat(loader.checkLoadable("  ")).isFalse();
+        assertThat(loader.checkLoadable("config.xml")).isFalse();
+        assertThat(loader.checkLoadable("config_properties.xml")).isFalse();
+        assertThat(loader.checkLoadable("config_properties.yml")).isFalse();
+
+    }
 }
