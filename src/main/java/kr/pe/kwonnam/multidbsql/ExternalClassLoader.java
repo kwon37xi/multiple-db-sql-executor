b1aa2217d0d6200a69ac140d95589297c3b42747
KwonNam Son <kwon37xi@gmail.com>
2016-05-17 23:15:17 +0900

    implement classloader

---
+++ src/main/java/kr/pe/kwonnam/multidbsql/ExternalClassLoader.java
@@ -0,0 +1,54 @@
+package kr.pe.kwonnam.multidbsql;
+
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ExternalClassLoader {
+    private List<URL> urls;
+    private URLClassLoader urlClassLoader;
+
+    public ExternalClassLoader(List<String> classpaths) {
+        if (CollectionUtils.isEmpty(classpaths)) {
+            throw new IllegalArgumentException("classpaths must not be empty.");
+        }
+        initializeUrls(classpaths);
+        initializeUrlClassLoader();
+    }
+
+    private void initializeUrls(List<String> classpaths) {
+        urls = new ArrayList<>();
+
+
+        try {
+            for (String classpath : classpaths) {
+                urls.add(new File(classpath).toURI().toURL());
+            }
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void initializeUrlClassLoader() {
+        urlClassLoader = new URLClassLoader(urls.toArray(new URL[] {}), this.getClass().getClassLoader());
+    }
+
+    public List<URL> getUrls() {
+        return Collections.unmodifiableList(urls);
+    }
+
+    public Class<?> loadClass(String className) {
+        try {
+            return urlClassLoader.loadClass(className);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(className + " is not available.", e);
+        }
+    }
+}
---
+++ src/test/java/kr/pe/kwonnam/multidbsql/ExternalClassLoaderTest.java
@@ -0,0 +1,66 @@
+package kr.pe.kwonnam.multidbsql;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+
+public class ExternalClassLoaderTest {
+    private Logger log = LoggerFactory.getLogger(ExternalClassLoaderTest.class);
+
+    @Test
+    public void constructor_param_null() throws Exception {
+        try {
+            new ExternalClassLoader(null);
+            failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
+        } catch (IllegalArgumentException ex) {
+            assertThat(ex).hasMessage("classpaths must not be empty.");
+        }
+    }
+
+    @Test
+    public void constructor_param_empty() throws Exception {
+        try {
+            new ExternalClassLoader(new ArrayList<>());
+            failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
+        } catch (IllegalArgumentException ex) {
+            assertThat(ex).hasMessage("classpaths must not be empty.");
+        }
+    }
+
+    @Test
+    public void getUrls() throws Exception {
+        List<String> classpaths = new ArrayList<>();
+        classpaths.add("src/test/resources/h2-1.3.176.jar");
+        classpaths.add("/some/where/test.jar");
+
+        ExternalClassLoader externalClassLoader = new ExternalClassLoader(classpaths);
+
+        final List<URL> urls = externalClassLoader.getUrls();
+        assertThat(urls).hasSize(2)
+            .contains(
+                new File("src/test/resources/h2-1.3.176.jar").toURI().toURL(),
+                new File("/some/where/test.jar").toURI().toURL()
+            );
+    }
+
+    @Test
+    public void loadClass() throws Exception {
+        List<String> classpaths = new ArrayList<>();
+        classpaths.add("src/test/resources/h2-1.3.176.jar");
+
+        ExternalClassLoader externalClassLoader = new ExternalClassLoader(classpaths);
+        final Class<?> h2Driver = externalClassLoader.loadClass("org.h2.Driver");
+        assertThat(h2Driver).isNotNull().matches(aClass -> aClass.getCanonicalName().equals("org.h2.Driver"), "loaded class must be org.h2.Driver");
+    }
+}
